3cae667cffeab4b26e47e426ced71690f663d83c
M Faried <m.a.faried@gmail.com>
2022-07-24 15:59:43 +0200

    Added the deployment of the updated contracts.

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
-// migrating the appropriate contracts
+// // migrating the appropriate contracts
+var Oraclize = artifacts.require("../contracts/Oraclize.sol");
+var ERC721Mintable = artifacts.require("../contracts/ERC721Mintable.sol");
 var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
+  deployer.deploy(Oraclize);
+  deployer.deploy(ERC721Mintable);
   deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+  // deployer.deploy(SolnSquareVerifier);
 };

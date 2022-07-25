324f6fbec584b729a91497eec8a1608f1dfd79d6
M Faried <m.a.faried@gmail.com>
2022-07-25 23:59:24 +0200

    Inculded the SolnVerifier to the deployment.

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,11 +2,11 @@
 var Oraclize = artifacts.require("../contracts/Oraclize.sol");
 var ERC721Mintable = artifacts.require("../contracts/ERC721Mintable.sol");
 var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(Oraclize);
   deployer.deploy(ERC721Mintable);
-  deployer.deploy(SquareVerifier);
-  // deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(SquareVerifier)
+    .then(() => deployer.deploy(SolnSquareVerifier, SquareVerifier.address));
 };
